22bf502ca721d1b3d39accbbf45854230ca6b8f3
David <dave@pyne.com>
2026-04-29 07:35:05 -0500

    Add try-catch to Phaser game creation

--- web/src/game/phaser/createGame.ts
+++ web/src/game/phaser/createGame.ts
@@ -4,24 +4,29 @@ import { BootScene } from './scenes/BootScene';
 import { MapScene } from './scenes/MapScene';
 
 export function createGame(parent: HTMLDivElement, controller: GameController) {
-  return new Phaser.Game({
-    type: Phaser.AUTO,
-    parent,
-    backgroundColor: '#17130e',
-    scale: {
-      mode: Phaser.Scale.RESIZE,
-      autoCenter: Phaser.Scale.CENTER_BOTH,
-      width: parent.clientWidth || 1280,
-      height: parent.clientHeight || 720,
-    },
-    input: {
-      windowEvents: false,
-    },
-    render: {
-      antialias: false,
-      pixelArt: true,
-      roundPixels: true,
-    },
-    scene: [new BootScene(), new MapScene(controller)],
-  });
+  try {
+    return new Phaser.Game({
+      type: Phaser.AUTO,
+      parent,
+      backgroundColor: '#17130e',
+      scale: {
+        mode: Phaser.Scale.RESIZE,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: parent.clientWidth || 1280,
+        height: parent.clientHeight || 720,
+      },
+      input: {
+        windowEvents: false,
+      },
+      render: {
+        antialias: false,
+        pixelArt: true,
+        roundPixels: true,
+      },
+      scene: [new BootScene(), new MapScene(controller)],
+    });
+  } catch (e) {
+    console.error('Phaser game creation error:', e);
+    throw e;
+  }
 }
